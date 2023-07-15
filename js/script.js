14e983dea40ef2a1b5f228f957b198b60f555f59
jhree333 <jhree333@gmail.com>
2023-07-15 18:00:22 +0900

    add: JS 코드 리팩토링 + css 스타일 작성

--- js/script.js
+++ js/script.js
@@ -2,49 +2,90 @@ let playerCount = 0;
 let computerCount = 0;
 
 const getComputerChoice = () => {
+  const choices = ["가위", "바위", "보"];
   const randomNum = Math.floor(Math.random() * 3);
-  if (randomNum == 0) {
-    return "가위";
-  } else if (randomNum == 1) {
-    return "바위";
-  } else {
-    return "보";
-  }
+  return choices[randomNum];
 };
 
 const playRound = (playerSelection, computerSelection) => {
-  const resultTemplate = `사용자: ${playerSelection} \n컴퓨터: ${computerSelection} \n`;
   if (
     (playerSelection === "가위" && computerSelection === "보") ||
     (playerSelection === "바위" && computerSelection === "가위") ||
     (playerSelection === "보" && computerSelection === "바위")
   ) {
     playerCount += 1;
-    return `${resultTemplate}사용자가 이겼습니다.`;
+    return `사용자가 이겼습니다.`;
   } else if (playerSelection === computerSelection) {
-    return `${resultTemplate}비겼습니다.`;
+    return `비겼습니다.`;
   } else {
     computerCount += 1;
-    return `${resultTemplate}컴퓨터가 이겼습니다.`;
+    return `컴퓨터가 이겼습니다.`;
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    // prettier-ignore
-    const playerSelection = prompt("가위바위보를 입력하세요.")
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-    alert(result);
-  }
-  const countTemplate = `사용자 점수:${playerCount} \n컴퓨터 점수:${computerCount} \n`;
-  if (playerCount > computerCount) {
-    alert(`${countTemplate}승자: 사용자`);
-  } else if (playerCount === computerCount) {
-    alert(`${countTemplate}무승부`);
+const createElement = (text, className, result) => {
+  const element = document.createElement("p");
+  element.textContent = text;
+  element.setAttribute("class", className);
+  result.appendChild(element);
+};
+
+const createCountElement = (className, count, result) => {
+  const element = document.createElement("p");
+  element.textContent = `${className} 점수: ${count}`;
+  element.setAttribute("class", className);
+  result.insertBefore(element, result.firstChild);
+};
+
+function createResultElement(playerCount, computerCount, result) {
+  if (playerCount < 5 && computerCount < 5) {
+    createCountElement("컴퓨터", computerCount, result);
+    createCountElement("사용자", playerCount, result);
   } else {
-    alert(`${countTemplate}승자: 컴퓨터`);
+    const roundWinner = document.createElement("div");
+    roundWinner.setAttribute("class", "roundWinner");
+    createCountElement("컴퓨터", computerCount, result);
+    createCountElement("사용자", playerCount, result);
+    roundWinner.textContent =
+      playerCount > computerCount
+        ? "사용자가 최종 승리하였습니다."
+        : "컴퓨터가 최종 승리하였습니다.";
+    result.insertBefore(roundWinner, result.firstChild);
+  }
+  return result;
+}
+
+const playGame = (userChoice) => {
+  const result = document.createElement("div");
+  result.setAttribute("class", "result");
+
+  const countDiv = document.querySelectorAll(".result");
+  if (countDiv.length > 0) {
+    document.body.removeChild(countDiv[0]);
+  }
+
+  const playerSelection = userChoice;
+  createElement(`사용자: ${playerSelection}`, "user", result);
+
+  const computerSelection = getComputerChoice();
+  createElement(`컴퓨터: ${computerSelection}`, "computer", result);
+
+  const winner = playRound(playerSelection, computerSelection);
+  createElement(winner, "winner", result);
+
+  const scoreResult = createResultElement(playerCount, computerCount, result);
+  document.body.appendChild(scoreResult);
+
+  if (playerCount === 5 || computerCount === 5) {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
   }
 };
 
-game();
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playGame(e.target.textContent);
+  });
+});
